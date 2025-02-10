154dd95b7096f3b08ee8f5ce9cb1724ef35b2007
Karolina Groszewska <karolina.groszewska@gmail.com>
2025-02-10 17:24:51 -0600

    Initial Commit

---
+++ Scout/ScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutApp.swift
+//  Scout
+//
+//  Created by Kari Groszewska on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
